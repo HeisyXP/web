45d193340ae3b0e22be9f363e64294c3a61e51d9
Akshaya Raghuvanshi <scaledrop@gmail.com>
2024-05-19 13:41:19 +0530

    image converted to webp

--- components/hero.js
+++ components/hero.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Container from "./container";
-import heroImg from "../public/img/car.jpg";
+import heroImg from "../public/img/car.webp";
 
 const Hero = () => {
   return (
