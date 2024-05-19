0fceabd00f95e6afc246b81f1263c6a151f062b3
Akshaya Raghuvanshi <scaledrop@gmail.com>
2024-05-19 12:38:53 +0530

    fix: logo path

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
 import Image from "next/image"
-import Logo from "/public/logo.png";
+import Logo from "../public/logo.png";
 import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
